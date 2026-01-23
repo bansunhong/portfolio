02c8c71776ca4f630388ce5dfda51f3a1b800cc0
bansunhong <ashbsh@nate.com>
2026-01-24 06:21:35 +0900

    test render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-function App() {
-  return <h1>HELLO WORLD</h1>;
-}
+import React, { memo } from "react";
+
+import { Routes, Route } from "react-router-dom";
+
+import { Reset } from "styled-reset";
+import MyGlobalStyles from "./components/MyGlobalStyles";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Main";
+
+const App = memo(() => {
+    return (
+        <>
+            <Reset />
+            <MyGlobalStyles />
+
+            <Header />
+            <Routes>
+                <Route path="/" exact={true} element={<Main />} />
+            </Routes>
+            <Footer />
+        </>
+    );
+});
 
 export default App;
-
